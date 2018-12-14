058720c96498af425507b6cde1aa1bcad2849bf4
grantbaker <grant.baker@colorado.edu>
2018-12-14 14:04:49 -0700

    Adjustments

--- scripts/sawyer_kinect_irl.py
+++ scripts/sawyer_kinect_irl.py
@@ -34,7 +34,42 @@ traj_options.interpolation_type = TrajectoryOptions.CARTESIAN
 
 listener = tf.TransformListener()
 
-rate = rospy.Rate(4.0) # SUGGEST - Try 10 or more
+
+
+BASE_X = 0.5
+BASE_Y = -0.3
+BASE_Z = 0.2
+OR_X = 1.0
+OR_Y = 0.0
+OR_Z = 0.0
+OR_W = 0.0
+SCALING = 0.3
+
+pose_goal = Pose()
+
+pose_goal.orientation.w = OR_W
+pose_goal.orientation.x = OR_X
+pose_goal.orientation.y = OR_Y
+pose_goal.orientation.z = OR_Z
+pose_goal.position.x = BASE_X
+pose_goal.position.y = BASE_Y
+pose_goal.position.z = BASE_Z
+
+poseStamped = PoseStamped()
+poseStamped.pose = pose_goal
+
+
+traj = MotionTrajectory(trajectory_options = traj_options, limb = limb)
+
+joint_angles = limb.joint_ordered_angles()
+waypoint = MotionWaypoint(options = wpt_opts.to_msg(), limb = limb)
+waypoint.set_cartesian_pose(poseStamped, 'right_hand', joint_angles)
+
+traj.append_waypoint(waypoint.to_msg())
+
+result = traj.send_trajectory(timeout=5.0)
+
+rate = rospy.Rate(30.0) # SUGGEST - Try 10 or more
 while not rospy.is_shutdown():
     try:
         (trans, rot) = listener.lookupTransform('kinect/user_1/torso', 'kinect/user_1/right_hand', rospy.Time(0))
@@ -43,15 +78,15 @@ while not rospy.is_shutdown():
         rate.sleep()
         continue
 
-    print "setting goal state: " + str(trans)
+    rospy.loginfo( "setting goal state: " + str(trans))
 
     pose_goal = Pose()
-    pose_goal.orientation.w = 0.0
-    pose_goal.orientation.x = 0.0
-    pose_goal.orientation.y = 1.0
-    pose_goal.orientation.z = 0.0
+    pose_goal.orientation.w = OR_W
+    pose_goal.orientation.x = OR_X
+    pose_goal.orientation.y = OR_Y
+    pose_goal.orientation.z = OR_Z
 
-    trans = [0.2 * trans[0] + 0.6, -0.2 * trans[2] + 0.3, 0.2 * trans[1] + 0.2]
+    trans = [SCALING * trans[1] + BASE_X, SCALING * trans[2] + BASE_Y, SCALING * trans[0] + BASE_Z]
 
     pose_goal.position.x = trans[0]
     pose_goal.position.y = trans[1]
--- scripts/sawyer_kinect_sim.py
+++ scripts/sawyer_kinect_sim.py
@@ -29,9 +29,33 @@ display_trajectory_publisher = rospy.Publisher('/move_group/display_planned_path
                                                moveit_msgs.msg.DisplayTrajectory,
                                                queue_size=20)
 
+group.set_planning_time(5)
+
+BASE_X = 0.5
+BASE_Y = 0.3
+BASE_Z = 0.2
+OR_X = 0.0
+OR_Y = 0.0
+OR_Z = -0.0
+OR_W = 0.0
+SCALING = 0.3
+
+pose_goal = gm.Pose()
+pose_goal.orientation.w = OR_W
+pose_goal.orientation.x = OR_X
+pose_goal.orientation.y = OR_Y
+pose_goal.orientation.z = OR_Z
+pose_goal.position.x = BASE_X
+pose_goal.position.y = BASE_Y
+pose_goal.position.z = BASE_Z
+group.set_pose_target(pose_goal)
+
+group.stop()
+plan = group.go(wait=True)
+
 group.set_planning_time(1)
 
-rate = rospy.Rate(2.0)
+rate = rospy.Rate(5.0)
 while not rospy.is_shutdown():
     try:
         (trans, rot) = listener.lookupTransform('kinect/user_1/torso', 'kinect/user_1/right_hand', rospy.Time(0))
@@ -42,8 +66,11 @@ while not rospy.is_shutdown():
     print "setting goal state: " + str(trans)
 
     pose_goal = gm.Pose()
-    pose_goal.orientation.w = 1.0
-    trans = [0.3 * trans[0] + 0.3, 0.3 * trans[2]+0.3, 0.8*trans[1] + 0.2]
+    pose_goal.orientation.w = OR_W
+    pose_goal.orientation.x = OR_X
+    pose_goal.orientation.y = OR_Y
+    pose_goal.orientation.z = OR_Z
+    trans = [SCALING*trans[0] + BASE_X, SCALING*trans[2] + BASE_Y, SCALING*trans[1] + BASE_Z]
     pose_goal.position.x = trans[0]
     pose_goal.position.y = trans[1]
     pose_goal.position.z = trans[2]
